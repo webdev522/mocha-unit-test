e26ae3865c6cbc07279c1fee04b4d1289a264320
webdev522 <webdev522@gmail.com>
2019-02-08 15:24:15 +0800

    initial commit

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+    });
+  });
+});
+
+describe('Math', function() {
+  it('should test if 3 * 3 = 9', function() {
+    assert.equal(9, 3 * 3);
+  });
+  it('should test if (3 - 4) * 8 = -8', function() {
+    assert.equal(-8, (3 - 4) * 8);
+  });
+});
